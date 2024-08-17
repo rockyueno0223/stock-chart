89ce6fa67d42473696d0dd2801c779c432854133
Hiroki Ueno <rockyueno@gmail.com>
2024-08-16 21:35:45 -0700

    install chart.js

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import ChartComponent from './components/ChartComponent.vue'
 </script>
 
 <template>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ChartComponent/>
 </template>
 
 <style scoped>
---
+++ src/components/ChartComponent.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import Chart from 'chart.js/auto';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const ctx = document.getElementById('myChart') as HTMLCanvasElement | null;
+  if (ctx) {
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+});
+</script>
+<template>
+  <h2>Chart</h2>
+  <canvas id="myChart"></canvas>
+</template>

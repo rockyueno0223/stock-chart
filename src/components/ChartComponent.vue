196162a7f59df71c4abd067cd1c7da55bd2709dc
Hiroki Ueno <rockyueno@gmail.com>
2024-08-19 13:15:44 -0700

    created chart with time range

--- src/components/ChartComponent.vue
+++ src/components/ChartComponent.vue
@@ -5,87 +5,157 @@ import { onMounted, ref, computed } from 'vue';
 let stockHistoryPrices: number[] = [];
 let stockHistoryDates: string[] = [];
 
-let stockSymbol = ref('IBM');
 const apiKey = import.meta.env.VITE_API_KEY;
+
+let stockSymbol = ref('IBM');
+let dateRange = ref('max')
+
 let apiUrl = computed(() => {
-  return `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${stockSymbol.value}&apikey=${apiKey}`;
+  if (dateRange.value === 'max' || dateRange.value === '5years') {
+    return `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${stockSymbol.value}&apikey=${apiKey}`;
+  } else if (dateRange.value === '1year') {
+    return `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=${stockSymbol.value}&apikey=${apiKey}`;
+  } else if(dateRange.value === '1month'){
+    return `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol.value}&interval=60min&outputsize=full&extended_hours=false&apikey=${apiKey}`;
+  } else {
+    return '';
+  }
 })
 
-onMounted(() => {
+let chartInstance: Chart | null = null;
+
+const createChartWithDate = async (inputValue: string) => {
   const ctx = document.getElementById('myChart') as HTMLCanvasElement | null;
   if (ctx) {
-    const createChartWithDate = async () => {
-      try {
-        // fetch stock price data and push to array
-        const response = await fetch(apiUrl.value);
-        const data = await response.json();
+    try {
+      // destroy existing chart
+      if (chartInstance) {
+        chartInstance.destroy();
+      }
 
-        console.log(data);
-        console.log(data['Meta Data']['2. Symbol']);
-        console.log(Object.keys(data['Monthly Adjusted Time Series'])[0]);
-        console.log(data['Monthly Adjusted Time Series']['1999-12-31']);
+      // fetch stock price data and push to array
+      dateRange.value = inputValue;
 
-        stockHistoryDates = [];
-        stockHistoryPrices = [];
+      const response = await fetch(apiUrl.value);
+      const data = await response.json();
 
+      console.log(data);
+
+      let sortedData: { [key: string] : any } = {};
+
+      if (dateRange.value === 'max') {
+        sortedData = data['Monthly Adjusted Time Series'];
+      } else if (dateRange.value === '5years') {
+        const monthsInFiveYears: number = 60;
+        let counter = 0;
         for (let property in data['Monthly Adjusted Time Series']) {
-          const formattedStockHistoryDate = `${property.split('-')[1]}/${property.split('-')[2]}/${property.split('-')[0]}`
-          stockHistoryDates.unshift(formattedStockHistoryDate);
-
-          stockHistoryPrices.unshift(Number(data['Monthly Adjusted Time Series'][property]['4. close']));
+          if (counter < monthsInFiveYears) {
+            sortedData[property] = data['Monthly Adjusted Time Series'][property];
+          } else {
+            break;
+          }
+          counter++
         }
+      } else if (dateRange.value === '1year') {
+        const weeksInOneYear: number = 52;
+        let counter = 0;
+        for (let property in data['Weekly Adjusted Time Series']) {
+          if (counter < weeksInOneYear) {
+            sortedData[property] = data['Weekly Adjusted Time Series'][property];
+          } else {
+            break;
+          }
+          counter++
+        }
+      } else if (dateRange.value === '1month') {
+        sortedData = data['Time Series (60min)'];
+      }
 
-        // create chart with fetched data
-        new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: stockHistoryDates,
-            datasets: [{
-              label: 'Stock Market Price',
-              data: stockHistoryPrices,
-              borderWidth: 1,
-              borderColor: 'red'
-            }]
+      stockHistoryDates = [];
+      stockHistoryPrices = [];
+
+      for (let property in sortedData) {
+        let formattedStockHistoryDate = '';
+        if (dateRange.value === '1month') {
+          const propertyDate = property.split(' ')[0];
+          const propertyTime = property.split(' ')[1];
+          formattedStockHistoryDate = `${propertyDate.split('-')[1]}/${propertyDate.split('-')[2]} ${propertyTime.split(':')[0]}:${propertyTime.split(':')[1]}`;
+        } else {
+          formattedStockHistoryDate = `${property.split('-')[1]}/${property.split('-')[2]}/${property.split('-')[0]}`;
+        }
+        stockHistoryDates.unshift(formattedStockHistoryDate);
+
+        stockHistoryPrices.unshift(Number(sortedData[property]['4. close']));
+      }
+
+      // create chart with fetched data
+      chartInstance = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: stockHistoryDates,
+          datasets: [{
+            label: 'Stock Market Price',
+            data: stockHistoryPrices,
+            borderWidth: 1,
+            borderColor: 'red'
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: false
+            }
           },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true
-              }
-            },
-            elements: {
-              point: {
-                pointStyle: false
-              }
-            },
-            plugins: {
-              tooltip: {
-                callbacks: {
-                  label: function(context) {
-                    let label = context.dataset.label || '';
+          elements: {
+            point: {
+              pointStyle: false
+            }
+          },
+          plugins: {
+            tooltip: {
+              callbacks: {
+                label: function (context) {
+                  let label = context.dataset.label || '';
 
-                    if (label) {
-                      label += ': ';
-                    }
-                    if (context.parsed.y !== null) {
-                      label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
-                    }
-                    return label;
+                  if (label) {
+                    label += ': ';
                   }
+                  if (context.parsed.y !== null) {
+                    label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
+                  }
+                  return label;
                 }
               }
             }
           }
-        });
-      } catch (error) {
-        console.error('Error fetching data and creating chart', error);
-      }
+        }
+      });
+    } catch (error) {
+      console.error('Error fetching data and creating chart', error);
     }
-    createChartWithDate();
   }
+};
+
+onMounted(() => {
+  createChartWithDate('max');
 });
 </script>
-<template>
-  <h2>Chart</h2>
-  <canvas id="myChart"></canvas>
+
+<template >
+  <div class="w-full flex flex-col justify-center">
+    <h2>Chart</h2>
+    <form action="" id="search-form" class="w-full">
+      <input type="text" name="search-form-symbol" id="search-form-symbol" :value="stockSymbol" class="border border-black rounded">
+      <div class="w-full flex justify-center">
+        <button type="button" @click="createChartWithDate('1month')" class="border border-black rounded mx-1 px-2">1 Month</button>
+        <button type="button" @click="createChartWithDate('1year')" class="border border-black rounded mx-1 px-2">1 Year</button>
+        <button type="button" @click="createChartWithDate('5years')" class="border border-black rounded mx-1 px-2">5 Years</button>
+        <button type="button" @click="createChartWithDate('max')" class="border border-black rounded mx-1 px-2">Max</button>
+      </div>
+    </form>
+    <p>Stock Symbol: {{ stockSymbol }}</p>
+    <p>date range: {{ dateRange }}</p>
+    <p>api url: {{ apiUrl }}</p>
+    <canvas id="myChart"></canvas>
+  </div>
 </template>
